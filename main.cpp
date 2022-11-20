e847f97462bb2c7fd3c9550c586321dbe2192fac
MABobrov <118075278+MABobrov@users.noreply.github.com>
2022-11-20 19:04:52 +0300

    with status code

--- main.cpp
+++ main.cpp
@@ -9,27 +9,27 @@ int main()  {
     else {
         if ((login == 3333) && (pass == 4444)) printf("welcome second_brother");
         else {
-            if ((login == 0000) && (pass == 0000)) {
+            if ((login == 0000) && (pass == 0000)) //регистрация нового пользователя
+            {
                 int new_user_login, new_user_pass;
                 printf("new_user_login=");
                 scanf("%i", &new_user_login);
                 printf("new_user_pass=");
                 scanf("%i", &new_user_pass);
-
+                     //закрываем возможность использования занятых логопасов
                 if (((new_user_login == 1111) || (new_user_pass == 2222)) ||
                     ((new_user_login == 3333) || (new_user_pass == 4444)) ||
                     ((new_user_login == 0000) || (new_user_pass == 0000)))
                     printf("login or pass exist");
-
                  else{
                     if ((new_user_login>login)||(new_user_pass > pass))
                         printf("hello, %i", new_user_login);
-                    return 0;
+                    return 0;//данные уникальны завершение со статус кодом = 0
                 }
             } else {
 
                 printf("Authorization denied");
-                return 1;
+                return 1;//попытка входа с не валидными кредами - Отказ + завершение со статус кодом=1
             }
 
         }
